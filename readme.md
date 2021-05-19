c2355bdb04ae592d0d8f6b73f8eeb70393b87c9c
NicoledeHaan <58775312+NicoledeHaan@users.noreply.github.com>
2021-05-19 21:45:55 +0200

    Add files via upload

--- readme.md
+++ readme.md
@@ -1,41 +1,65 @@
-<!-- Vergeet je niet de comments uit te zetten voordat je begint met typen? 💬 -->
-
 # Procesverslag
 
 ## Over
-* **Naam:** `voornaam + achternaam`
-* **Klas:** `vid-1/vid-2`
-* **Minor:** `Visual Interface Design`
-* **Favoriete dier:** `Zeeotters zijn te gek!`
-* **Startniveau:** `blauw/rood/zwart`
+Naam: Nicole de Haan
+Klas: VID-1
+Minor: Visual Interface Design
 
 ## Concept
-
-`Beschrijf in het algemeen je concept van je project.`
+Ik heb Amsterdamse huizen gemaakt met ramen met een mooie gradient achtergrond voor dag en nacht (met sterren).
 
 ## Features
+Generatieve:
+Elke keer dat je de pagina refreshed krijgen de daken van de huizen een andere vorm en grootte en de ramen krijgen een andere grootte en kleur en zie je de sterren veranderen. 
 
-`Wat zijn de features in het project dat je gemaakt hebt? Waar kan ik op klikken? Waar zit de interactie?`
+Gebruikers input:
+Je kan doormiddel van de slider de kleur van de achtergrond veranderen, en zelf ramen toevoegen aan de huizen.
 
 ## Onderzoek & inspiratie
-`Schrijf wat over je onderzoek en je inspiratie voor het project. Hoe kwam je concept tot stand?`
+Het eerste idee wat bij mij opkwam was om iets met cirkcels te doen en die tegen elkaar aan te laten gaan. Maar dit heeft uiteraard niet zoveel met mij persoonlijk te maken. Vervolgens ben ik na gaan denken wat nou echt bij mij past. Ik ben een geboren en getogen Amsterdammer. Altijd als ik in de stad loop hou ik van de grachten en huizen daar. Zo ben ik op het idee gekomen om Amsterdamse huizen te maken. Verder vond ik het een beetje saai om dat als enige te doen, ik ben gaan denken waar hou ik nog meer van. Terwijl ik dit aan het bedenken was zat ik in de ochtend op mijn balkon met een mooie zonsopgang. Ik maakte een foto (want dat doe ik altijd als ik een mooie zonsopgang of zonsondergang zie) en dacht direct dat ga ik doen! Ik ga een achtergrond maken die verandert van dag naar nacht.  
 
 ## Voortang
-
-`Schrijf hier een klein logboekje met je voortgang per week.`
+Hieronder vind je mijn voortgang per week. 
 
 ### Week-1
-`Wat heb je gedaan? Wat ging goed? Wat kon beter?`
+In de eerste week had ik al wat gemaakt in p5 met cirkels maar dit was niet iets dat bij mij paste. Vervolgens ben ik gaan denken wat dan wel bij mijn past en heb besloten om Amsterdamse huizen te maken (zie onderzoek en inspiratie voor volledige uitleg). Ik ben als eerst lijnen gaan maken voor de huizen en gele vierkantjes voor de ramen.
+
+Ik kon inplaatsvan gelijk te beginnen beter eerst nadenken wat ik precies wilde gaan maken, want nu had ik al iets gemaakt in p5 wat ik uiteindelijk helemaal niet ging gebruiken. Verder ging het maken van de lijnen van huizen en de vierkanten erg goed!
+
 
 ### Week-2
-`Wat heb je gedaan? Wat ging goed? Wat kon beter?`
+In de tweede week wilde ik de huizen een kleur geven ipv alleen een omlijning. Ik merkte doordat ik lijnen had gebruikt dit niet kon dus moest de huizen helemaal opnieuw gaan maken met vlakken. Ik heb gekozen voor de kleur grijs van de huizen, zodat dit rustig oogt en niet teveel afleidt. 
+
+Ik heb hier feedback op gekregen van Danny, ik liep vooral vast bij het maken van een gradient, want overal zag ik dat ik met lijnen moest werken en dit een border moest meegeven maar ik kreeg dit niet voor elkaar. Danny gaf aan dat ik het beste een vlak kon maken en die een kleur moest geven.
+
+Ik denk dat ik mijn concept direct helemaal moest uitdenken want dan had ik kunnen voorkomen dat ik de huizen helemaal opnieuw moest maken. Verder ging het eigenlijk na de feedback van Danny erg soepel!
 
 ### Week-3
-`Wat heb je gedaan? Wat ging goed? Wat kon beter?`
+In de derde week ben ik vooral bezig geweest met mijn gradient. Het lukte om een gradient te maken, alleen zag je nog wel de lijnen door de huizen heen, dit heb ik kunnen weghalen door de border weg te halen. 
 
+Ik liep erg vast hoe ik dan een gradient achtergrond moest maken van dag naar nacht. Verder had ik met hulp van Danny mijn gradient achtergrond laten veranderen op basis van tijd met een if else statement. Dit ging erg goed!
+
+Ik denk dat ik uiteindelijk de achtergrond zonder hulp van Danny ook zelf had kunnen veranderen op basis van tijd, omdat ik wist dat ik moest zeggen if die tijd dan die achtergrond else die achtergrond alleen vond ik het lastig om precies te weten wat mijn code dan zou worden, maar achteraf was dat helemaal niet zo lastig.
+
+### Week-4
+In de vierde week kreeg ik feedback van Danny dat ik het beste een slider kon gebruiken voor het veranderen van de achtergrond, zodat ik gebruikersinput heb en dat Danny niet de hele dag hoeft te wachten tot mijn kunstwerk verandert. 
+
+Het maken van de slider verliep vrij soepel! Alleen kreeg ik deze niet werkend met mijn achtergrond. Danny heeft mij hierbij geholpen om hem van zwart naar wit te krijgen. Zelf ben ik hier nog lang mee bezig geweest en uiteindelijk is het mij gelukt om mijn gradient te laten werken op basis van de slider.
+
+Achteraf had ik de if else statement met tijd ophalen helemaal niet nodig, dit kostte me veel tijd en ik kwam er niet uit zonder Danny dus vond het erg zonde dat dit niet meer nodig was.
+
+### Week-5
+In de laatste week kreeg ik feedback van Danny dat ik mijn kunstwerk nog niet generatief is. Ik heb uiteindelijk ervoor gekozen om de ramen te laten veranderen van grootte en kleur en de daken van de huizen te laten veranderen van grootte en vorm. 
+
+Vervolgens wilde ik nog meer gebruikersinput en ik wilde dat de gebruiker ook zelf ramen kon tekenen, ik liep hier erg vast want Linda Klaver had mij haar code gestuurd en zij had precies wat ik wilde. Ik heb haar code deels overgenomen maar het werkte niet. Na uren zoeken kwam ik erachter dat dat te maken had met dat ik het veranderen van de ramen in mijn draw had met een noloop ipv in mijn setup. Toen ik dat had verandert werkte het. 
 
 ## Bronnenlijst
 
-* `Link naar bron 1`
-* `Link naar bron 2`
-* `Link naar bron 3`
+* Bron sunset achtergrond: https://editor.p5js.org/Jaemi13/sketches/gAS-FB5Sx
+* Bron voor sterren: https://codeburst.io/sunsets-and-shooting-stars-in-p5-js-92244d238e2b 
+* Bron voor de huizen: https://www.youtube.com/watch?v=riZB_qCQXtI
+* Bron random: https://happycoding.io/tutorials/p5js/random
+* Bron mousepressed: https://www.youtube.com/watch?v=asvnmtVjeEk
+* Bron ramen toevoegen: https://editor.p5js.org/LindaKlaver/sketches/7qnbwB3PC/
+* Bron random driehoek: https://editor.p5js.org/awade5/sketches/rkWIYwPsX
+* Bron gradient op slider: Sjors Wijsman derdejaars CMD student
--- sketch.js
+++ sketch.js
@@ -1,14 +1,436 @@
-/* 👇 Start writing your p5.js code here */
+/*Constants voor sunset achtergrond*/
+let box = [];
+let colors = ['#ffa500'];
+let slider;
+let sunsetC1;
+let sunsetC2;
+let nightC1;
+let nightC2;
+var x;
+var y;
+var x1;
+var y1;
+var x2;
+var y2;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(displayWidth, displayHeight);
+
+  slider = createSlider(0, 1, 0, 0.01);
+  slider.position(650, 650);
+  slider.changed(drawScene);
+  
+  sunsetC1 = color(115, 194, 251);
+  sunsetC2 = color(100, 147, 5);
+  nightC1 = color(0, 0, 35);  
+  nightC2 = color(222, 186, 139);
+  
+  drawScene()
 }
 
-function draw() {
-  background(220);
-  ellipse(width / 2, height / 2, 50, 50);
+function draw(){
+          for (let i = 0; i < box.length; i++) {
+    box[i].display();
+    }
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+function drawScene() {
+  let c1 = lerpColor(sunsetC1, nightC1, slider.value());
+  let c2 = lerpColor(sunsetC2, nightC2, slider.value());
+  
+  createGradient(c1, c2);
+  
+  drawHouses();
+}
+
+function createGradient(c1, c2) {
+  for(let y = 0; y < height; y++){
+    n = map(y, 0, height, 0, 1);
+    let newc = lerpColor(c1, c2, n);
+    stroke(newc);
+    line(0, y, width, y);
+  }
+}
+
+function drawHouses() {
+    /*Bron voor sterren: https://codeburst.io/sunsets-and-shooting-stars-in-p5-js-92244d238e2b */
+    /*Sterren bovenin het scherm, wisselt als je refresht*/
+    for (var i = 0; i < 50; i++) {
+    var x = random(windowWidth);
+    var y = random(windowHeight-200);
+    noStroke();
+    fill(255, 255, 0);
+    ellipse(x, y, 2, 2);
+    }
+    
+    /*Bron voor de huizen: https://www.youtube.com/watch?v=riZB_qCQXtI*/
+    /*huis1*/
+    fill(102);
+    rect(100, 200, 200, 400);
+    fill(102);
+    
+    /*Bron random driehoek: https://editor.p5js.org/awade5/sketches/rkWIYwPsX*/
+    x =random(200,200);
+    y =random(20,180);
+    x1 =random(100, 100);
+    x2  =random(100,100);
+    y1 =random(200,200);
+    x2 =random(300,300);
+    y2 =random(200,200);
+    triangle(x,y,x1,y1,x2,y2);
+  
+    /*Bron random: /*https://happycoding.io/tutorials/p5js/random*/
+    /*Kleur van ramen huis 1*/
+    const colors10 = ['yellow', 'red', 'blue'];
+    fill(random(colors10));
+    /*Vlakken van ramen huis 1*/
+    var rectX = random(100, 150);
+    var recty = random(200, 250);
+    var rectSize = random (30, 40);
+    rect(rectX, recty, rectSize);
+    
+    var rectX = random(200, 250);
+    var recty = random(200, 250);
+    var rectSize = random (30, 40);
+    rect(rectX, recty, rectSize);
+
+    var rectX = random(100, 150);
+    var recty = random(300, 350);
+    var rectSize = random (30, 40);
+    rect(rectX, recty, rectSize);
+    
+    var rectX = random(200, 250);
+    var recty = random(300, 350);
+    var rectSize = random (30, 40);
+    rect(rectX, recty, rectSize);
+    
+    var rectX = random(100, 150);
+    var recty = random(400, 450);
+    var rectSize = random (30, 40);
+    rect(rectX, recty, rectSize);
+    
+    var rectX = random(200, 250);
+    var recty = random(400, 450);
+    var rectSize = random (30, 40);
+    rect(rectX, recty, rectSize);
+
+    var rectX = random(100, 150);
+    var recty = random(500, 550);
+    var rectSize = random (30, 40);
+    rect(rectX, recty, rectSize);
+    
+    var rectX = random(200, 250);
+    var recty = random(500, 550);
+    var rectSize = random (30, 40);
+    rect(rectX, recty, rectSize);
+    
+    /*huis2*/
+    fill(102);
+    rect(310, 200, 200, 400);
+    noStroke();
+    fill(102);
+    var rectX20 = random(360, 360);
+    var recty20 = random(150, 150);
+    var rectSize20 = random (60, 100);
+    rect(rectX20, recty20, rectSize20);
+    
+    /*Kleur van ramen huis 2*/
+    const colors2 = ['pink', 'purple', 'green'];
+    fill(random(colors2));
+    /*Vlakken van ramen huis 2*/
+    var rectX2 = random(310, 360);
+    var recty2 = random(200, 250);
+    var rectSize2 = random (30, 40);
+    rect(rectX2, recty2, rectSize2);
+    
+    var rectX2 = random(410, 460);
+    var recty2 = random(200, 250);
+    var rectSize2 = random (30, 40);
+    rect(rectX2, recty2, rectSize2);
+
+    var rectX2 = random(310, 360);
+    var recty2 = random(300, 350);
+    var rectSize2 = random (30, 40);
+    rect(rectX2, recty2, rectSize2);
+    
+    var rectX2 = random(410, 460);
+    var recty2 = random(300, 350);
+    var rectSize2 = random (30, 40);
+    rect(rectX2, recty2, rectSize2);
+    
+    var rectX2 = random(310, 360);
+    var recty2 = random(400, 450);
+    var rectSize2 = random (30, 40);
+    rect(rectX2, recty2, rectSize2);
+    
+    var rectX1 = random(410, 460);
+    var recty1 = random(400, 450);
+    var rectSize1 = random (30, 40);
+    rect(rectX1, recty1, rectSize1);
+
+    var rectX2 = random(310, 360);
+    var recty2 = random(500, 550);
+    var rectSize2 = random (30, 40);
+    rect(rectX2, recty2, rectSize2);
+    
+    var rectX2 = random(410, 460);
+    var recty2 = random(500, 550);
+    var rectSize2 = random (30, 40);
+    rect(rectX2, recty2, rectSize2);
+    
+    /*huis3*/
+    fill(102);
+    rect(520, 200, 200, 400);
+    
+    fill(102);
+    var rectX30 = random(570, 570);
+    var recty30 = random(150, 150);
+    var rectSize30 = random (80, 150);
+    rect(rectX30, recty30, rectSize30);
+    
+    fill(102);
+    noStroke();
+    var rectX40 = random(595, 595);
+    var recty40 = random(110, 110);
+    var rectSize40 = random (50, 60);
+    rect(rectX40, recty40, rectSize40);
+    
+    /*Kleur van ramen huis 3*/
+    const colors1 = ['yellow', 'red', 'blue'];
+    fill(random(colors1));
+    /*Vlakken van ramen huis 3*/
+    var rectX1 = random(520, 570);
+    var recty1 = random(200, 250);
+    var rectSize1 = random (30, 40);
+    rect(rectX1, recty1, rectSize1);
+    
+    var rectX1 = random(620, 670);
+    var recty1 = random(200, 250);
+    var rectSize1 = random (30, 40);
+    rect(rectX1, recty1, rectSize1);
+
+    var rectX1 = random(520, 570);
+    var recty1 = random(300, 350);
+    var rectSize1 = random (30, 40);
+    rect(rectX1, recty1, rectSize1);
+    
+    var rectX1 = random(620, 670);
+    var recty1 = random(300, 350);
+    var rectSize1 = random (30, 40);
+    rect(rectX1, recty1, rectSize1);
+    
+    var rectX1 = random(520, 570);
+    var recty1 = random(400, 450);
+    var rectSize1 = random (30, 40);
+    rect(rectX1, recty1, rectSize1);
+    
+    var rectX1 = random(620, 670);
+    var recty1 = random(400, 450);
+    var rectSize1 = random (30, 40);
+    rect(rectX1, recty1, rectSize1);
+
+    var rectX1 = random(520, 570);
+    var recty1 = random(500, 550);
+    var rectSize1 = random (30, 40);
+    rect(rectX1, recty1, rectSize1);
+    
+    var rectX1 = random(620, 670);
+    var recty1 = random(500, 550);
+    var rectSize1 = random (30, 40);
+    rect(rectX1, recty1, rectSize1);
+    
+    /*huis4*/
+    fill(102);
+    rect(730, 200, 200, 400);
+    
+    fill(102);
+    noStroke();
+    x =random(830,830);
+    y =random(20,180);
+    x1 =random(730, 730);
+    x2  =random(730,730);
+    y1 =random(200,200);
+    x2 =random(930,930);
+    y2 =random(200,200);
+    triangle(x,y,x1,y1,x2,y2);
+  
+    
+   /*Kleur van ramen huis 4*/
+    const colors3 = ['pink', 'purple', 'green'];
+    fill(random(colors3));
+    /*Vlakken van ramen huis 4*/
+    var rectX3 = random(730, 780);
+    var recty3 = random(200, 250);
+    var rectSize3 = random (30, 40);
+    rect(rectX3, recty3, rectSize3);
+    
+    var rectX3 = random(830, 880);
+    var recty3 = random(200, 250);
+    var rectSize3 = random (30, 40);
+    rect(rectX3, recty3, rectSize3);
+
+    var rectX3 = random(730, 780);
+    var recty3 = random(300, 350);
+    var rectSize3 = random (30, 40);
+    rect(rectX3, recty3, rectSize3);
+    
+    var rectX3 = random(830, 880);
+    var recty3 = random(300, 350);
+    var rectSize3 = random (30, 40);
+    rect(rectX3, recty3, rectSize3);
+    
+    var rectX3 = random(730, 780);
+    var recty3 = random(400, 450);
+    var rectSize3 = random (30, 40);
+    rect(rectX3, recty3, rectSize3);
+    
+    var rectX3 = random(830, 880);
+    var recty3 = random(400, 450);
+    var rectSize3 = random (30, 40);
+    rect(rectX3, recty3, rectSize3);
+
+    var rectX3 = random(730, 780);
+    var recty3 = random(500, 550);
+    var rectSize3 = random (30, 40);
+    rect(rectX3, recty3, rectSize3);
+    
+    var rectX3 = random(830, 880);
+    var recty3 = random(500, 550);
+    var rectSize3 = random (30, 40);
+    rect(rectX3, recty3, rectSize3);
+    
+    /*huis5*/
+    fill(102);
+    rect(940, 200, 200, 400);
+    
+    fill(102);
+    noStroke();
+    var rectX50 = random(990, 990);
+    var recty50 = random(130, 130);
+    var rectSize50 = random (80, 150);
+    rect(rectX50, recty50, rectSize50);
+    
+   /*Kleur van ramen huis 5*/
+    const colors4 = ['yellow', 'red', 'blue'];
+    fill(random(colors4));
+    /*Vlakken van ramen huis 5*/
+    var rectX4 = random(950, 1000);
+    var recty4 = random(200, 250);
+    var rectSize4 = random (30, 40);
+    rect(rectX4, recty4, rectSize4);
+    
+    var rectX4 = random(1050, 1090);
+    var recty4 = random(200, 250);
+    var rectSize4 = random (30, 40);
+    rect(rectX4, recty4, rectSize4);
+
+    var rectX4 = random(950, 1000);
+    var recty4 = random(300, 350);
+    var rectSize4 = random (30, 40);
+    rect(rectX4, recty4, rectSize4);
+    
+    var rectX4 = random(1050, 1090);
+    var recty4 = random(300, 350);
+    var rectSize4 = random (30, 40);
+    rect(rectX4, recty4, rectSize4);
+    
+    var rectX4 = random(950, 1000);
+    var recty4 = random(400, 450);
+    var rectSize4 = random (30, 40);
+    rect(rectX4, recty4, rectSize4);
+    
+    var rectX4 = random(1050, 1090);
+    var recty4 = random(400, 450);
+    var rectSize4 = random (30, 40);
+    rect(rectX4, recty4, rectSize4);
+
+    var rectX4 = random(950, 1000);
+    var recty4 = random(500, 550);
+    var rectSize4 = random (30, 40);
+    rect(rectX4, recty4, rectSize4);
+    
+    var rectX4 = random(1050, 1090);
+    var recty4 = random(500, 550);
+    var rectSize4 = random (30, 40);
+    rect(rectX4, recty4, rectSize4);
+    
+    /*huis6*/
+    fill(102);
+    rect(1150, 200, 200, 400);
+  
+    fill(102);
+    var rectX30 = random(1200, 1200);
+    var recty30 = random(140, 140);
+    var rectSize30 = random (100, 150);
+    rect(rectX30, recty30, rectSize30);
+    
+    fill(102);
+    noStroke();
+    var rectX40 = random(1225, 1225);
+    var recty40 = random(90, 90);
+    var rectSize40 = random (50, 90);
+    rect(rectX40, recty40, rectSize40);
+    
+   /*Kleur van ramen huis 6*/
+    const colors5 = ['pink', 'purple', 'green'];
+    fill(random(colors5));
+    /*Vlakken van ramen huis 6*/
+    var rectX5 = random(1150, 1200);
+    var recty5 = random(200, 250);
+    var rectSize5 = random (30, 40);
+    rect(rectX5, recty5, rectSize5);
+    
+    var rectX5 = random(1250, 1300);
+    var recty5 = random(200, 250);
+    var rectSize5 = random (30, 40);
+    rect(rectX5, recty5, rectSize5);
+
+    var rectX5 = random(1150, 1200);
+    var recty5 = random(300, 350);
+    var rectSize5 = random (30, 40);
+    rect(rectX5, recty5, rectSize5);
+    
+    var rectX5 = random(1250, 1300);
+    var recty5 = random(300, 350);
+    var rectSize5 = random (30, 40);
+    rect(rectX5, recty5, rectSize5);
+    
+    var rectX5 = random(1150, 1200);
+    var recty5 = random(400, 450);
+    var rectSize5 = random (30, 40);
+    rect(rectX5, recty5, rectSize5);
+    
+    var rectX5 = random(1250, 1300);
+    var recty5 = random(400, 450);
+    var rectSize5 = random (30, 40);
+    rect(rectX5, recty5, rectSize5);
+
+    var rectX5 = random(1150, 1200);
+    var recty5 = random(500, 550);
+    var rectSize5 = random (30, 40);
+    rect(rectX5, recty5, rectSize5);
+    
+    var rectX5 = random(1250, 1300);
+    var recty5 = random(500, 550);
+    var rectSize5 = random (30, 40);
+    rect(rectX5, recty5, rectSize5);
+}
+
+/*Bron voor raam tekenen: https://editor.p5js.org/LindaKlaver/sketches/7qnbwB3PC */
+function mousePressed() {
+    box.push(new Box(mouseX, mouseY));
+}
+
+class Box {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.r = random(25);
+    }
+
+    display() {
+        noStroke();
+        fill(colors);
+        rect(this.x, this.y, this.r * 2, this.r * 2);
+    }
 }
